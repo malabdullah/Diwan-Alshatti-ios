f9f2fa285e221d806a264fa7efa11953e074f536
Mohamad Alabdullah <alabdullah.cs@gmail.com>
2016-05-30 00:19:08 -0400

    Initial Commit

---
+++ testSwiftyJSON/Members.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+class Member{
+    
+    var name:String?
+    var date:String?
+    var comment:String?
+    var id:String?
+    
+    init( name: String , date: String , id: String ) {
+        self.name = name
+        self.date = date
+        self.id = id
+    }
+    
+}
